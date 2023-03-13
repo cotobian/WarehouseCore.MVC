ffd5756cfea2ae6d69ee17f9a957db44fbd233b5
cotobian <cotobian@gmail.com>
2023-03-13 15:16:56 +0700

    Update 13/03

---
+++ WarehouseCore.MVC/Models/Validator/FunctionValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System.Collections.Generic;
+using WarehouseCore.MVC.Enums;
+
+namespace WarehouseCore.MVC.Models.Validator
+{
+    public class FunctionValidator : AbstractValidator<Function>
+    {
+        public FunctionValidator(ActionMethod method, IEnumerable<Function> functions)
+        {
+            RuleFor(x => x.Url).NotEmpty().WithMessage("Url không thể để trống!");
+            RuleFor(x => x.Url).MaximumLength(100).WithMessage("Url không thể dài hơn 100 ký tự!");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Tên chức năng không thể để trống!");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Tên chức năng không thể dài hơn 50 ký tự!");
+            RuleFor(x => x.SortOrder).NotEmpty().WithMessage("SortOrder không thể để trống!");
+            if (method == ActionMethod.Create)
+            {
+                RuleFor(x => x.Url).SetValidator(new UniqueValidator<Function>(functions)).WithMessage("Url đã tồn tại");
+                RuleFor(x => x.Name).SetValidator(new UniqueValidator<Function>(functions)).WithMessage("Tên chức năng đã tồn tại");
+            }
+        }
+    }
+}
---
+++ WarehouseCore.MVC/ViewModels/InventoryVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WarehouseCore.MVC.ViewModels
+{
+    public class InventoryVm
+    {
+        public string Shipment { get; set; }
+        public string POSO { get; set; }
+        public string Unit { get; set; }
+        public int Quantity { get; set; }
+        public int PositionId { get; set; }
+        public string PositionName { get; set; }
+    }
+}
